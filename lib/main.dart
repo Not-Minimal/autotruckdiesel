e7146e9ba4eb09e64aad591b03a81c49a091a856
Saul Muñoz <sam.pedreros14@gmail.com>
2022-09-29 17:10:18 -0300

    Actualizar

--- lib/main.dart
+++ lib/main.dart
@@ -31,11 +31,72 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       // ignore: prefer_const_constructors
       drawer: Drawer(
-        backgroundColor: Colors.black,
+        child: ListView(
+          padding: EdgeInsets.only(top: 120),
+          children: <Widget>[
+            ListTile(
+              leading: Icon(Icons.description_rounded),
+              title: const Text('Ordenes de Trabajo'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.list_alt_rounded),
+              title: const Text('Ordenes de Compra'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.assignment_rounded),
+              title: const Text('Informes'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.article_rounded),
+              title: const Text('Checklist'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.inventory_2_rounded),
+              title: const Text('Productos'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.groups_rounded),
+              title: const Text('Clientes'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.bar_chart_rounded),
+              title: const Text('Estadisticas'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.payments_rounded),
+              title: const Text('Producción'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.directions_car_rounded),
+              title: const Text('Vehiculos'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.receipt_long_rounded),
+              title: const Text('Facturas'),
+              onTap: () {},
+            ),
+          ],
+        ),
       ),
       appBar: AppBar(
-          title: const Text("Autotruck Diesel SpA"),
-          backgroundColor: Colors.amber),
+        title: Text('Autotruck Diesel SpA'),
+        actions: <Widget>[
+          IconButton(
+            icon: new Icon(Icons.search_rounded),
+            onPressed: () => print('hi on icon action'),
+          ),
+        ],
+      ),
+
       backgroundColor: Colors.black87,
       body: Center(
         child: Column(
